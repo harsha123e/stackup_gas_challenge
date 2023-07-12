5f874d23a2f6cc2ca0231eb610fff6529d6b6131
Harsha <nsriharsha3@gmail.com>
2023-07-12 10:20:37 +0530

    Add working gas challenge files

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract gasChallenge {
-    //Implement Fixed-Size Array Technique Here
-    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
-    
+    // Using a fixed-size array allocates memory for the array upfront, resulting in reduced gas consumption
+    uint[10] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
     //Function to check for sum of array
     //No changes required in this function
     function getSumOfArray() public view returns (uint256) {
@@ -14,16 +14,34 @@ contract gasChallenge {
         }
         return sum;
     }
-    
+
     function notOptimizedFunction() public {
         for (uint i = 0; i < numbers.length; i++) {
             numbers[i] = 0;
         }
     }
-    
-    //Implement Remaining Gas Optimization Techniques Here
+
+    //Implementing Remaining Gas Optimization Techniques Here
     //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-      
+        // Cache the state variable accessing the local array
+        // Accessing the local array instead of the state variable inside the loop reduces gas consumption
+        uint[10] memory cachedNumbers = numbers;
+
+        // Use unchecked block to skip overflow checks 
+        // The loop that sets each element to 0 is enclosed within the unchecked block. 
+        // This allows the contract to skip expensive overflow checks, resulting in reduced gas consumption
+        unchecked {
+            for (uint i = 0; i < cachedNumbers.length; i++) {
+                cachedNumbers[i] = 0;
+            }
+        }
+       
+        // Assign the updated values back to the state variable
+        // Using ++i syntax can potentially optimize gas consumption in certain situations
+        // as it avoids an additional operation to store the incremented value
+        for (uint i = 0; i < numbers.length; ++i) {
+            numbers[i] = cachedNumbers[i]; 
+        }
     }
- }
+}
--- test/test_gasChallenge.js
+++ test/test_gasChallenge.js
@@ -21,6 +21,7 @@ describe("Deploy Gas Challenge Contract", () => {
   describe("Check Sum Of Array", () => {
     it("Should return 0", async () => {
       // Write test block here to check sum of array equals 0
+      await gas_contract.getSumOfArray();
     });
   });
 });
